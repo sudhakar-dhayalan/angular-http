cc6107ccd0b782cbd0cbb1e636f88c03a0025346
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2020-10-18 11:17:46 +0530

    http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  data;
+
+  constructor(private http: HttpClient) {
+    let params = new HttpParams().set('id', '6');
+
+    //Method 1 : Using pipe and async in html template (Angular way)
+    //this.data = this.http.get('https://jsonplaceholder.typicode.com/posts');
+
+    //Method 2 : Using subscribe (Typescript or JavaScript way)
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts', {params})
+      .subscribe((value: any) =>
+        this.data = value
+      );
+
+    console.log(this.http.get('https://jsonplaceholder.typicode.com/posts'));//http returns Observable
+  }
+
   title = 'angular-http';
 }
